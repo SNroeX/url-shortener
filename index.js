65caffc378a16d8d57f7a31e66e039599d6431d0
SNroeX <pradhannirvik2017@gmail.com>
2026-08-04 17:57:04 +0530

    Day 2 - Added routes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
