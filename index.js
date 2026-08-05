52176a9f91c81b55837c123e1ade03f15b8df8d3
SNroeX <pradhannirvik2017@gmail.com>
2026-08-05 11:39:35 +0530

    Day 3 - Added POST route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.send("About Page");
 });
+app.post("/shorten", (req, res) => {
+    res.send("URL received!");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
